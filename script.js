92f70887e3d9ab09cc010d335e7a80b18d6757b7
Aleksei Anufriev <axoft10@gmail.com>
2022-05-17 20:21:34 +0700

    Settings

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+/**
+ * Объект DTO с настройками игры по умолчанию, которые можно будет поменять при инициализации игры.
+ * @property {int} rowsCount Количество строк.
+ * @property {int} colsCount Количество колонок.
+ * @property {int} speed Скорость змейки.
+ * @property {int} winLength Длина змейки для победы.
+ */
+
+ const settings = {
+    rowsCount: 21,
+    colsCount: 21,
+    speed: 2,
+    winFoodCount: 50,
+  };
+  
